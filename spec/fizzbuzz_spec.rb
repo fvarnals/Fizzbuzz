6b0558acc61907ba3d14b0b0b6ae3b7f059755ac
fvarnals <f.varnals@gmail.com>
2019-06-20 14:58:12 +0100

    Updated to include tests for each criterion

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,13 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
 	  expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns 7 when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
+  it 'returns "fizzbuzz" when passed a multiple of both 3 and 5' do
+    expect(fizzbuzz(rand(999999)*15)).to eq 'fizzbuzz'
+  end
 end
